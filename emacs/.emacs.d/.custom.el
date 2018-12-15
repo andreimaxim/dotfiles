41857e3d70f609d3da49949ab62d9f222594b447
Andrei Maxim <andrei@andreimaxim.ro>
2018-12-15 19:50:05 +0200

    WIP, again.

---
+++ emacs/.emacs.d/.custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (crux yard-mode which-key use-package smartparens ruby-tools ruby-test-mode rspec-mode robe restclient rbenv rake rainbow-delimiters neotree multi-term minitest magit ivy-hydra ibuffer-projectile highlight-numbers highlight-indentation gitignore-mode gitconfig-mode git-timemachine git-gutter-fringe ggtags flyspell-correct-ivy flycheck-pos-tip flycheck-color-mode-line flx fill-column-indicator enh-ruby-mode doom-themes doom-modeline dired-k counsel-projectile counsel-gtags company-quickhelp company-dict cider bundler better-defaults auto-package-update aggressive-indent ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
