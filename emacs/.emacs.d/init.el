1ca542e367aa13275f5b88720a520e1e7950019b
Andrei Maxim <andrei@andreimaxim.ro>
2018-12-15 09:55:54 +0200

    Moving to literate configuration of Emacs.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,16 @@
-;;; init.el -*- lexical-binding: t -*-
+;;; init.el --- Personal Emacs configuration -*- lexical-binding: t -*-
 ;;
-;; Author: Andrei Maxim
-;; URL:    https://github.com/andreimaxim/dotfiles/emacs
+;; Author:           Andrei Maxim
+;; URL:              https://github.com/andreimaxim/dotfiles/emacs
+;; Version:          0.0.1
+;; Package-Requires: ((emacs "26.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;; License: GPLv2
+;;
+
+;;; Commentary:
 ;;
 ;; Inspired by the configuration files of several people like:
 ;;
@@ -15,51 +20,57 @@
 ;;
 ;; ... and probably others I've seen on https://github.com/caisah/emacs.dz.
 
+;;; Code:
+;;
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;; (package-initialize)
 
-;; Bootstrap the configuration
-(require 'acm/init (concat user-emacs-directory "core/init"))
+;; Don't attempt to find/apply special file handlers to files loaded during startup.
+;; (let ((file-name-handler-alist nil))
+;;   ;; If config is pre-compiled, then load that
+;;   (if (file-exists-p (expand-file-name "README.elc" user-emacs-directory))
+;;       (load-file (expand-file-name "README.elc" user-emacs-directory))
+;;     ;; Otherwise use org-babel to tangle and load the configuration
+;;     (require 'org)
+;;     (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))))
 
-;; I honestly have no idea what I'm doing here.
-;;
-;; Most likely the whole `load' will crash fantastically and I will not have any
-;; idea why or how to fix it, but it works now so... I guess it's fine?
-;;
-;; PS: It's obviously not fine.
-;;
-;; TODO:
-;;
-;; 1. Move away from a per-package config file (see `which-key') and have more
-;;    logical blocks, like `completion'. I guess it should be fine to have
-;;    several files for ruby, clojure, etc.
-;;
-;; 2. Use `require' instead of `load' as it is highly recommended. Probably
-;;    having a prefix will avoid potential collisions.
-;;
-;; 3. Switch from Helm to Ivy + Swiper + Counsel.
+;; Don't attempt to find/apply special file handlers to files loaded during startup.
+;; (let ((file-name-handler-alist nil))
+;;   ;; If config is pre-compiled, then load that
+;;   (if (file-exists-p (expand-file-name "README.el" user-emacs-directory))
+;;       (load-file (expand-file-name "README.el" user-emacs-directory))
+;;     ;; Otherwise use org-babel to tangle and load the configuration
+;;     (require 'org)
+;;     (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))))
 
-;; Interface, including:
-;; * startup screen
-;; * typography
-;; * theme
-;; * general user experience
-(require 'acm/ux (concat acm-modules-dir "ux"))
+(require 'org)
+(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-;; General tools, like:
-;; * completion
-;; * navigating files
-;; * moving through windows
-;; * working with git
-(require 'acm/tools (concat acm-modules-dir "tools"))
+;; ;; Bootstrap the configuration
+;; (require 'acm/init (concat user-emacs-directory "core/init"))
 
-;; Context-specific settings
-;; (require 'acm/org     (concat acm-modules-dir "org"))
-(require 'acm/ruby    (concat acm-modules-dir "ruby"))
-(require 'acm/clojure (concat acm-modules-dir "clojure"))
-;; (require 'acm/elisp)
-;; (require 'acm/data)     ; JSON and friends
-;; (require 'acm/blogging) ; Jekyll or something like it 
+;; ;; I honestly have no idea what I'm doing here.
+
+;; ;; Interface, including:
+;; ;; * startup screen
+;; ;; * typography
+;; ;; * theme
+;; ;; * general user experience
+;; (require 'acm/ux (concat acm-modules-dir "ux"))
+
+;; ;; General tools, like:
+;; ;; * completion
+;; ;; * navigating files
+;; ;; * moving through windows
+;; ;; * working with git
+;; (require 'acm/tools (concat acm-modules-dir "tools"))
+
+;; ;; Context-specific settings
+;; (require 'acm/ruby    (concat acm-modules-dir "ruby"))
+;; (require 'acm/clojure (concat acm-modules-dir "clojure"))
+;; ;; (require 'acm/elisp)
+;; ;; (require 'acm/data)     ; JSON and friends
+;; ;; (require 'acm/blogging) ; Jekyll or something like it 
