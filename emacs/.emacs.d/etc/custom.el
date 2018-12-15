641c5092beeba1c03164cb2453728c8b3ceeb39f
Andrei Maxim <andrei@andreimaxim.ro>
2018-12-15 10:32:14 +0200

    Minor tweaks

--- emacs/.emacs.d/etc/custom.el
+++ emacs/.emacs.d/etc/custom.el
@@ -3,16 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
- '(package-selected-packages
-   (quote
-    (ruby-tools ruby-test-mode magit flycheck company auto-package-update rcodetools yard minitest ivy-hydra restclient nlinum counsel-projectile counsel-gtags flyspell-correct-ivy doom-themes flx doom-modeline swiper counsel ivy company-dict yard-mode which-key use-package try spaceline-all-the-icons smex smartparens rubocop rspec-mode robe rbenv rake rainbow-delimiters pretty-mode popwin poet-theme orgit neotree multi-term ibuffer-projectile highlight-numbers highlight-indentation helm-projectile helm-gtags helm-company helm-ag golden-ratio gitignore-mode gitconfig-mode git-timemachine git-link git-gutter-fringe ggtags flyspell-correct-helm flymake-ruby flycheck-pos-tip fill-column-indicator enh-ruby-mode dracula-theme dockerfile-mode docker-compose-mode dired-k dash-functional company-statistics company-quickhelp company-inf-ruby color-theme-sanityinc-tomorrow cider bundler better-defaults all-the-icons-dired alert aggressive-indent ag ace-window)))
  '(tooltip-mode -1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
