1628fab997f223c8d4d022c50d1d28118eb570e2
Andrei Maxim <andrei@andreimaxim.ro>
2024-06-27 22:07:45 +0300

    Show all files in the explorer sans .git

---
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+   "neo-tree.nvim",
+   opts = {
+     filesystem = {
+       filtered_items = {
+
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        never_show = {
+          '.git'
+        }
+
+       }
+     }
+   }
+}
