4d18470304b403ba12e5dac84a6dbdad1a478aa9
Andrei Maxim <andrei@andreimaxim.ro>
2024-06-28 00:51:33 +0300

    Use ivy style everywhere

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,42 +1,60 @@
 return {
   "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    local theme = require("telescope.themes").get_ivy({
+      layout_config = {
+        height = 15,
+      },
+    })
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, theme, {
+      mappings = {
+        i = {
+          ["<esc>"] = "close",
+        },
+      },
+    })
+
+    opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
+      find_files = {
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true,
+        file_ignore_patterns = {
+          "^.git/",
+          "^node_modules/",
+        },
+      },
+      grep_string = {
+        additional_args = function()
+          return { "--hidden", "--no-ignore", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" }
+        end,
+      },
+    })
+
+    return opts
+  end,
   keys = {
     {
       "<leader><leader>",
       function()
-        local builtin = require("telescope.builtin")
-        local themes = require("telescope.themes")
-        builtin.find_files(themes.get_ivy({
-          hidden = true,
-          no_ignore = true,
-          no_ignore_parent = true,
-          file_ignore_patterns = {
-            "^.git/",
-            "^node_modules/",
-          },
-          layout_config = {
-            height = 15,
-          },
-        }))
+        require("telescope.builtin").find_files()
       end,
       desc = "Find all files",
     },
     {
       "<leader>fw",
       function()
-        local builtin = require("telescope.builtin")
-        local themes = require("telescope.themes")
-        builtin.grep_string(themes.get_ivy({
-          additional_args = function()
-            return { "--hidden", "--no-ignore", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" }
-          end,
-          search = vim.fn.expand("<cword>"),
-          layout_config = {
-            height = 15,
-          },
-        }))
+        require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
       end,
       desc = "Find word under cursor",
+    },
+    {
+      "<leader>,",
+      function()
+        require("telescope.builtin").resume()
+      end,
+      desc = "Resume last picker",
     },
   },
 }
