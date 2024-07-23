0af53c5e2f50e79aff151e0c24c1b23a2d72a838
Andrei Maxim <andrei@andreimaxim.ro>
2024-07-23 14:55:29 +0300

    Configure ruby-lsp to always work with the current Ruby version

--- neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,16 +4,17 @@ return {
 
     opts = {
       servers = {
-        -- disable solargraph
+        -- Disable the default LSP server for Ruby
         solargraph = {
           autostart = false,
         },
 
         ruby_lsp = {
+          -- Make sure that the ruby-lsp is loaded for the current Ruby version
+          cmd = { "mise", "x", "--", "ruby-lsp" },
           on_attach = function(client, bufnr)
             -- Disable semantic tokens (syntax highlighting from LSP)
             client.server_capabilities.semanticTokensProvider = nil
-
             -- Disable document highlighting
             client.server_capabilities.documentHighlightProvider = false
           end,
